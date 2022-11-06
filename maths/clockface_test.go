2874aeec7e99747ae7bf676e9ea15a80eb2884b0
linguangliang <linguangliang@ifanr.com>
2022-11-06 10:45:35 +0800

    feat: add code

--- maths/clockface_test.go
+++ maths/clockface_test.go
@@ -25,16 +25,16 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.SecondHand(tm)
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
 
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v")
-// 	}
-// }
+	if got != want {
+		t.Errorf("Got %v, wanted %v")
+	}
+}
 
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
